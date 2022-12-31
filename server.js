ca0ef270dffcebb1ba17408c556f2754719d5b71
shamonhashmi <shamonhashmi148@gmail.com>
2023-01-01 03:30:27 +0530

    Assignment done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const PORT = 4000
+const PORT = process.env.PORT ||  4000
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
